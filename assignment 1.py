097edcac9bdb10f6c39124345c4e05f3d7d4fd03
jairajdua7-sketch <jairajdua7@gmail.com>
2025-09-14 00:10:23 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,5 @@
+a=input('Enter your first name: ')
+b=input('Enter your second name: ')
+print()
+c=a + " " + b
+print("Hello, "+ c +" ! Welcome to the Python world")
---
+++ simple.py
@@ -0,0 +1,10 @@
+a=float(input("Enter the first number: " ))
+b=float(input("Enter the second number: " ))
+print()
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print('Addition: ' ,c)
+print('Subtraction: ' ,d)
+print('Multiplication: ' ,e)
